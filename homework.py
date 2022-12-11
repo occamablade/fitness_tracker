05dd905ed0dc51597b95a449c23d8d0610e9803f
Ivan <bezborodnikov18@gmail.com>
2022-12-11 08:41:45 +0500

    last try

--- homework.py
+++ homework.py
@@ -84,12 +84,11 @@ class SportsWalking(Training):
         self.height = height
 
     def get_spent_calories(self) -> float:
-        return (self.CALORIES_WEIGHT_MULTIPLIER
-                * self.weight + self.get_mean_speed()
-                * self.KMH_IN_MSEC ** 2
-                / self.height / self.CM_IN_M
-                * self.CALORIES_SPEED_HEIGHT_MULTIPLIER
-                * self.weight) * self.duration * self.MIN_IN_H
+        return ((self.CALORIES_WEIGHT_MULTIPLIER * self.weight
+                + ((self.get_mean_speed() * self.KMH_IN_MSEC)**2
+                 / self.height * self.CM_IN_M)
+                 * self.CALORIES_SPEED_HEIGHT_MULTIPLIER * self.weight)
+                * self.duration * self.MIN_IN_H)
 
 
 class Swimming(Training):
@@ -113,8 +112,8 @@ class Swimming(Training):
                 / self.M_IN_KM / self.duration)
 
     def get_spent_calories(self) -> float:
-        return (self.get_mean_speed()
-                + self.CALORIES_MEAN_SPEED_SHIFT
+        return ((self.get_mean_speed()
+                + self.CALORIES_MEAN_SPEED_SHIFT)
                 * self.CALORIES_WEIGHT_MULTIPLIER
                 * self.weight * self.duration)
 
